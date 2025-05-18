6257caf675f3fbcbec59e49d3a9899538cf02947
Gian <gian78367@gmail.com>
2025-05-18 18:10:28 -0300

    arquivos finalizados

---
+++ Makefile
@@ -0,0 +1,18 @@
+CC = cc
+CFLAGS = -Wall -g
+
+all: programa
+
+programa: principal.o helloworld.o
+	$(CC) $(CFLAGS) -o principal.o helloworld.o
+
+principal.o: principal.c
+	$(CC) $(CFLAGS) -c principal.c
+
+helloworld.o: helloworld.c
+	$(CC) $(CFLAGS) -c helloworld.c
+
+clean:
+	rm -f *.o programa
+
+.PHONY: all clean
---
+++ principal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int n;
+    printf("Insira o número de execuções: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        pid_t pid; 
+        pid = fork();
+
+        if (pid == 0) {
+            printf("Execução do pai\n");
+            execl("./hello", "hello", NULL);
+            perror("execl falhou");
+            exit(1);
+        }
+        
+        if (pid > 0) {
+            printf("Execução do pai\n");
+        }
+
+        if (pid < 0) {
+            perror("fork falhou");
+        }
+    }
+
+    return 0;
+}
