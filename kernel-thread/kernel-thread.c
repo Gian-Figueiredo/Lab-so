8e25e3f8b0758282200fbc79a9769bc7bb5a76e5
Gian Victor Gonçalves Figueiredo <rolbel139@gmail.com>
2025-06-06 19:16:11 -0300

    Add files via upload

--- kernel-thread/kernel-thread.c
+++ kernel-thread/kernel-thread.c
@@ -1,75 +1,85 @@
-#include<linux/module.h>     // included for all kernel modules
-#include<linux/init.h>       // included for __init and __exit macros
-#include<linux/kthread.h>    // included for threading related functions
-#include<linux/sched.h>      // included to create task_struct
-#include<linux/delay.h>      // included for the sleep/delay function in the thread
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
 
-// array for task_struct to hold task info
-static struct task_struct kth_arr[4];
+#define NUM_THREADS 4
 
-// long running function to be executed inside a thread, this will run for 30 secs. 
-int thread_function(void * idx) {
-  unsigned int i = 0;
-  int t_id = * (int * ) idx;
+static struct task_struct *kth_arr[NUM_THREADS];
 
-  // kthread_should_stop call is important.
-  while (!kthread_should_stop()) {
-    printk(KERN_INFO "Thread %d Still running...! %d secs\n", t_id, i);
-    i++;
-    if (i == 30)
-      break;
-    msleep(1000);
-  }
-  printk(KERN_INFO "thread %d stopped\n", t_id);
-  return 0;
+int thread_function(void *idx) {
+    unsigned int i = 0;
+    int t_id = *(int *)idx;
+
+    printk(KERN_INFO "Thread %d iniciado com PID: %d\n", t_id, current->pid);
+
+    while (!kthread_should_stop()) {
+        printk(KERN_INFO "Thread %d ainda rodando... %d segundos\n", t_id, i);
+        i++;
+        if (i == 30)
+            break;
+        msleep(1000);
+    }
+
+    printk(KERN_INFO "Thread %d parando (PID: %d)\n", t_id, current->pid);
+    return 0;
 }
 
-// initialize one thread at a time.
-int initialize_thread(struct task_struct * kth, int idx) {
-  char th_name[20];
-  sprintf(th_name, "kthread_%d", idx);
-  kth = kthread_create(thread_function, & idx, (const char * ) th_name);
-  if (kth != NULL) {
-    wake_up_process(kth);
-    printk(KERN_INFO "%s is running\n", th_name);
-  } else {
-    printk(KERN_INFO "kthread %s could not be created\n", th_name);
-    return -1;
-  }
-  return 0;
+int initialize_thread(int idx) {
+    char th_name[20];
+    int *thread_id;
+
+    // alocar ID dinamicamente para cada thread
+    thread_id = kmalloc(sizeof(int), GFP_KERNEL);
+    if (!thread_id)
+        return -ENOMEM;
+
+    *thread_id = idx;
+    sprintf(th_name, "kthread_%d", idx);
+
+    kth_arr[idx] = kthread_create(thread_function, (void *)thread_id, th_name);
+    if (IS_ERR(kth_arr[idx])) {
+        printk(KERN_ERR "Falha ao criar thread %d\n", idx);
+        kfree(thread_id);
+        return PTR_ERR(kth_arr[idx]);
+    }
+
+    wake_up_process(kth_arr[idx]);
+    printk(KERN_INFO "Thread %d (%s) está rodando\n", idx, th_name);
+    return 0;
 }
 
-// module init function
 static int __init mod_init(void) {
-  int i = 0;
-  printk(KERN_INFO "Initializing thread module\n");
-  for (i = 0; i < 4; i++) {
-	// initialize one thread at a time.
-    if (initialize_thread( & kth_arr[i], i) == -1) {
-      return -1;
+    int i;
+    printk(KERN_INFO "Iniciando módulo de threads\n");
+    for (i = 0; i < NUM_THREADS; i++) {
+        if (initialize_thread(i) < 0) {
+            printk(KERN_ERR "Erro ao inicializar thread %d\n", i);
+            return -1;
+        }
     }
-  }
-  printk(KERN_INFO "all of the threads are running\n");
-  return 0;
+    printk(KERN_INFO "Todas as threads foram iniciadas\n");
+    return 0;
 }
 
-// module exit function.
 static void __exit mod_exit(void) {
-  int i = 0;
-  int ret = 0;
-  printk(KERN_INFO "exiting thread module\n");
-  for (i = 0; i < 4; i++) {
-	// stop all of the threads before removing the module.
-    ret = kthread_stop( & kth_arr[i]);
-    if (!ret) {
-      printk("can't stop thread %d", i);
+    int i;
+    printk(KERN_INFO "Encerrando módulo de threads\n");
+    for (i = 0; i < NUM_THREADS; i++) {
+        if (kth_arr[i]) {
+            int ret = kthread_stop(kth_arr[i]);
+            if (ret != -EINTR) {
+                printk(KERN_INFO "Thread %d finalizada\n", i);
+            } else {
+                printk(KERN_WARNING "Não foi possível parar a thread %d\n", i);
+            }
+        }
     }
-  }
-  printk(KERN_INFO "stopped all of the threads\n");
+    printk(KERN_INFO "Todas as threads foram encerradas\n");
 }
 
 MODULE_LICENSE("GPL");
-
 
 module_init(mod_init);
 module_exit(mod_exit);
