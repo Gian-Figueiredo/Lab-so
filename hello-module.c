ee78dacb5db332b28e6d5069b286decc6203f3f0
Gian Victor Gonçalves Figueiredo <rolbel139@gmail.com>
2025-06-06 18:20:41 -0300

    Add files via upload

--- hello-module.c
+++ hello-module.c
@@ -1,31 +1,21 @@
-/*  hello-1.c - The simplest kernel module.
- *
- *  Copyright (C) 2001 by Peter Jay Salzman
- *
- *  08/02/2006 - Updated by Rodrigo Rubira Branco <rodrigo@kernelhacking.com>
- */
 
-/* Kernel Programming */
 #define MODULE
 #define LINUX
 
 
-#include <linux/module.h>  /* Needed by all modules */
-#include <linux/kernel.h>  /* Needed for KERN_ALERT */
+#include <linux/module.h> 
+#include <linux/kernel.h>  
 
-
-int init_module(void)
-{
-   printk("<1>Hello world 1.\n");
-	
-   // A non 0 return means init_module failed; module can't be loaded.
-   return 0;
+static int __init principio_de_tudo(void) {
+  printk("<1> Hello world 1.\n");
+  return 0;
 }
 
-
-void cleanup_module(void)
-{
+static void __exit no_fim_era_so_eu(void) {
   printk(KERN_ALERT "Goodbye world 1.\n");
-}  
+}
+
+module_init(principio_de_tudo);
+module_exit(no_fim_era_so_eu);
 
 MODULE_LICENSE("GPL");
